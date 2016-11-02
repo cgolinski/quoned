7909a8548c492c06e58b757c2f8258f456aec399
cgolinski <cgolinski@users.noreply.github.com>
2016-11-01 22:39:19 -0400

    initial commit, with react

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MenuBar from './MenuBar.js' ;
+
+class App extends Component {
+  render() {
+    return (
+      <MenuBar>
+      </MenuBar>
+    );
+  }
+}
+
+export default App;
